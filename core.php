916067946bfd72e79267e7f5c5a0793dc9103bb5
esmailireza <esmailireza20@gmail.com>
2024-02-21 16:16:24 +0330

    implement plugin and register style,script files

--- core.php
+++ core.php
@@ -15,6 +15,6 @@ defined('STT_PLUGIN_URL',plugin_dir_url(file:__FILE__));
 function stt_reqister-assets(){
     wp_register_style(handle:'stt-style',plugins_url(path:'scroll-to-top/assets/css/style.css'),deps:'',ver:'1.0.0');
     wp_enqueue_style(handle:'stt-style');
-wp_register_script(handle:'stt-main-js',src:STT_PLUGIN_URL.'assets/js/main.js'),['jquery'],in_footer:'true',ver:'1.0.0');
-wp_enqueue_script(handle:'stt-main-js');
+    wp_register_script(handle:'stt-main-js',src:STT_PLUGIN_URL.'assets/js/main.js'),['jquery'],in_footer:'true',ver:'1.0.0');
+    wp_enqueue_script(handle:'stt-main-js');
 }
