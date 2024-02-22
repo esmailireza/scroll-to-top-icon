6f58a44b4b0f12c44d7f727c0a3798894276e39b
esmailireza <esmailireza20@gmail.com>
2024-02-22 14:57:22 +0330

    add scroll style and functionality to project

--- view/front/scroll-to-top.php
+++ view/front/scroll-to-top.php
@@ -1,1 +1,8 @@
 <?php
+function scroll_to_top_layout()
+{
+return '<a id="back2Top" class="top-scroll" title="back to top" href="#"><span>&#8593;</span>
+    <i class="ti-arrow-up"></i></a>';
+}
+
+add_shortcode('scroll-to-top', scroll_to_top_layout());
